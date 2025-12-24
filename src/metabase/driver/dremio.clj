1c2fd5d4cc6d9b60fc7e0805d1302768176ec26c
Juliano Baggio di Sopra <juliano.baggio@gruppy.com.br>
2025-12-24 15:21:44 +0000

    fix: forçar visibilidade via information_schema e look_in_sources

--- src/metabase/driver/dremio.clj
+++ src/metabase/driver/dremio.clj
@@ -35,26 +35,12 @@
                               :connection-impersonation        false}]
   (defmethod driver/database-supports? [:dremio feature] [_driver _feature _db] supported?))
 
-;;; +----------------------------------------------------------------------------------------------------------------+
-;;; |                                             metabase.driver impls                                              |
-;;; +----------------------------------------------------------------------------------------------------------------+
-
 (defmethod driver/display-name :dremio [_] "Dremio")
-     
+
 ;;; +----------------------------------------------------------------------------------------------------------------+
-;;; ----------------------------------------------------- Impls ------------------------------------------------------
+;;; |                                         Configuração da Conexão                                               |
 ;;; +----------------------------------------------------------------------------------------------------------------+
 
-;; don't use the Postgres specified implementation for `describe-database` and `describe-table`
-(defmethod driver/describe-database :dremio
-           [& args]
-           (apply (get-method driver/describe-database :sql-jdbc) args))
-
-(defmethod driver/describe-table :dremio
-  [& args]
-  (apply (get-method driver/describe-table :sql-jdbc) args))
-
-                  
 (defmethod sql-jdbc.conn/connection-details->spec :dremio
   [_ {:keys [user password schema host port ssl]
       :or {user "dbuser", password "dbpassword", schema "", host "localhost", port 31010}
@@ -62,7 +48,8 @@
   (-> {:applicationName    config/mb-app-id-string
        :type :dremio
        :subprotocol "dremio"
-       :subname (str "direct=" host ":" port (if-not (str/blank? schema) (str ";schema=" schema)))
+       ;; AJUSTE PONTUAL: Adicionado ;look_in_sources=true para ignorar o bug de visibilidade dos Spaces
+       :subname (str "direct=" host ":" port (if-not (str/blank? schema) (str ";schema=" schema)) ";look_in_sources=true")
        :user user
        :password password
        :host host
@@ -73,26 +60,40 @@
        :sendTimeAsDatetime false}
       (sql-jdbc.common/handle-additional-options details, :seperator-style :semicolon)))
 
-;; Skip the postgres implementation  as it has to handle custom enums which dremio doesn't support. Based on redshift fix
+;;; +----------------------------------------------------------------------------------------------------------------+
+;;; |                                         Descoberta de Metadados                                               |
+;;; +----------------------------------------------------------------------------------------------------------------+
+
+;; AJUSTE PONTUAL: Usa jdbc/query com a spec do driver para listar tabelas via SQL, contornando o bug do JDBC padrão
+(defmethod driver/describe-database :dremio
+  [driver database]
+  {:tables
+   (set
+    (jdbc/query (sql-jdbc.conn/connection-details->spec driver (:details database))
+                ["SELECT TABLE_SCHEMA AS \"schema\", TABLE_NAME AS \"name\" FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA NOT IN ('sys', 'information_schema')"]))})
+
+(defmethod driver/describe-table :dremio
+  [& args]
+  (apply (get-method driver/describe-table :sql-jdbc) args))
+
+;;; +----------------------------------------------------------------------------------------------------------------+
+;;; |                                         Restante das Implementações                                            |
+;;; +----------------------------------------------------------------------------------------------------------------+
+
 (defmethod driver/dynamic-database-types-lookup :dremio
   [driver database database-types]
   ((get-method driver/dynamic-database-types-lookup :sql-jdbc) driver database database-types))
 
-
-;; custom Dremio type handling
 (def ^:private database-type->base-type
   (sql-jdbc.sync/pattern-based-database-type->base-type
-    [[#"(?i)DOUBLE" :type/Float]       ; Dremio uses DOUBLE for float8
-     [#"(?i)INTEGER" :type/Integer]    ; Dremio uses INTEGER for int4
-     ]))
+    [[#"(?i)DOUBLE" :type/Float]
+     [#"(?i)INTEGER" :type/Integer]]))
 
 (defmethod sql-jdbc.sync/database-type->base-type :dremio
   [driver column-type]
   (or (database-type->base-type column-type)
       ((get-method sql-jdbc.sync/database-type->base-type :postgres) driver (keyword (str/lower-case (name column-type))))))
 
-
-;; Dremio doesn't support "+ (INTERVAL '-30 day')"
 (defmethod sql.qp/add-interval-honeysql-form :dremio
   [_ hsql-form amount unit]
   [:timestampadd [:raw (name unit)] amount (h2x/->timestamp hsql-form)])
@@ -107,7 +108,6 @@
   [_ _ expr]
   (sql.qp/adjust-start-of-week :dremio (partial date-trunc :week) expr))
 
-;; bound variables are not supported in Dremio
 (defmethod driver/execute-reducible-query :dremio
   [driver {:keys [database settings], {sql :query, :keys [params], :as inner-query} :native, :as outer-query} context respond]
   (let [inner-query (-> (assoc inner-query
@@ -125,7 +125,6 @@
   (binding [driver/*compile-with-inline-parameters* true]
     ((get-method sql.qp/format-honeysql :postgres) driver honeysql-form)))
 
-;; Dremio's cast DateTime to STRING methods (when unprepare)
 (defmethod sql.qp/inline-value [:dremio OffsetDateTime]
   [_ t]
   (format "timestamp '%s'" (t/format "yyyy-MM-dd HH:mm:ss.SSS" t)))
@@ -134,11 +133,9 @@
   [driver t]
   (sql.qp/inline-value driver (t/offset-date-time t)))
 
-;; Dremio is always in UTC
 (defmethod driver/db-default-timezone :dremio [_ _]
            "UTC")
 
-;; Dremio's jdbc doesn't support getObject(Class<T> type)
 (prefer-method
   sql-jdbc.execute/read-column-thunk
   [::legacy/use-legacy-classes-for-read-and-set Types/TIMESTAMP]
